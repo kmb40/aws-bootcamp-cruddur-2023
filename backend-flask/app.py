522a9602a83c4f514903ed7bfa7694c78a13cdd2
Kyle M. Brown <mayvik@gmail.com>
2023-03-09 13:33:56 -0500

    Update app.py
    Edits for JWT Server Side Integration

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -157,7 +157,19 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 @xray_recorder.capture('activities') # xray sub segments
 def data_home():
-  data = HomeActivities.run()
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    # authenicatied request
+    app.logger.debug("authenicated")
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
+  except TokenVerifyError as e:
+    # unauthenicatied request
+    app.logger.debug(e)
+    app.logger.debug("unauthenicated")      
+    data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])

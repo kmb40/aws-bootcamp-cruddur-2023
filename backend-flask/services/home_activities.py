4d9bcd7d779ed7eb607b478645d4efe1ca68c6ec
Kyle M. Brown <mayvik@gmail.com>
2023-03-15 18:52:03 +0000

    SQL Query

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -16,7 +16,20 @@ class HomeActivities:
       span.set_attribute("app.now", now.isoformat()) #Honeycomb attribute
       
       sql = query_wrap_array("""
-      SELECT * FROM activities
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
       """)
       #span.set_attribute("app.result_length",len(results)) #Honeycomb 
       with pool.connection() as conn:

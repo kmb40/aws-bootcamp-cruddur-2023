129776053affb9ab459aef3683766f1bcb03c058
Kyle M. Brown <mayvik@gmail.com>
2023-03-21 00:47:13 +0000

    reintegrated notifications

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import HomeFeedPage from './pages/HomeFeedPage';
+import NotificationsFeedPage from './pages/NotificationsFeedPage';
 import UserFeedPage from './pages/UserFeedPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
@@ -38,6 +39,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeFeedPage />
+  },
+  {
+    path: "/notifications",
+    element: <NotificationsFeedPage />
   },
   {
     path: "/@:handle",

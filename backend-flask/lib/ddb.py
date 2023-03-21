f0cdacdd02979fc775ea82a9879c8ec3595faaac
Kyle M. Brown <mayvik@gmail.com>
2023-03-21 10:33:52 -0400

    Update ddb.py

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -5,7 +5,7 @@ import uuid
 import os
 import botocore.exceptions
 
-#Class definitions and Client
+# Class Definitions and Client
 class Ddb:
   def client():
     endpoint_url = os.getenv("AWS_ENDPOINT_URL")
@@ -15,3 +15,36 @@ class Ddb:
       attrs = {}
     dynamodb = boto3.client('dynamodb',**attrs)
     return dynamodb
+  
+# Controls Message Groups
+  def list_message_groups(client,my_user_uuid):
+    year = str(datetime.now().year)
+    table_name = 'cruddur-messages'
+    query_params = {
+      'TableName': table_name,
+      'KeyConditionExpression': 'pk = :pk AND begins_with(sk,:year)',
+      'ScanIndexForward': False,
+      'Limit': 20,
+      'ExpressionAttributeValues': {
+        ':year': {'S': year },
+        ':pk': {'S': f"GRP#{my_user_uuid}"}
+      }
+    }
+    print('query-params:',query_params)
+    print(query_params)
+    # query the table
+    response = client.query(**query_params)
+    items = response['Items']
+    
+
+    results = []
+    for item in items:
+      last_sent_at = item['sk']['S']
+      results.append({
+        'uuid': item['message_group_uuid']['S'],
+        'display_name': item['user_display_name']['S'],
+        'handle': item['user_handle']['S'],
+        'message': item['message']['S'],
+        'created_at': last_sent_at
+      })
+    return results

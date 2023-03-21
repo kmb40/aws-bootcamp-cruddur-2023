ac367e612b0d96d907f2658980b874d76c7ce174
Kyle M. Brown <mayvik@gmail.com>
2023-03-21 09:05:32 -0400

    Update ddb.py

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -1,1 +1,16 @@
+import boto3
+import sys
+from datetime import datetime, timedelta, timezone
+import uuid
+import os
+import botocore.exceptions
 
+class Ddb:
+  def client():
+    endpoint_url = os.getenv("AWS_ENDPOINT_URL")
+    if endpoint_url:
+      attrs = { 'endpoint_url': endpoint_url }
+    else:
+      attrs = {}
+    dynamodb = boto3.client('dynamodb',**attrs)
+    return dynamodb

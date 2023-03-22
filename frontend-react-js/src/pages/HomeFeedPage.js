b16a92c38cfbe8244e6c9445d3aa4349fbb2de74
Kyle M. Brown <mayvik@gmail.com>
2023-03-22 09:29:31 -0400

    Update HomeFeedPage.js

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -1,7 +1,7 @@
 import './HomeFeedPage.css';
 import React from "react";
 
-import checkAuth from '../lib/CheckAuth';
+import CheckAuth from '../lib/CheckAuth';
 import DesktopNavigation  from '../components/DesktopNavigation';
 import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';

77a12364f73690aeddc36cc24ba9d29349872b8d
Kyle M. Brown <mayvik@gmail.com>
2023-03-22 09:41:40 -0400

    Update MessageGroupPage.js

--- frontend-react-js/src/pages/MessageGroupPage.js
+++ frontend-react-js/src/pages/MessageGroupPage.js
@@ -41,7 +41,7 @@ export default function MessageGroupPage() {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${params.message_group_uuid}`
       const res = await fetch(backend_url, {
         headers: {
-          Authorization: 'Bearer ${localStorage.getItem("access_token")}'
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
         },
         method: "GET"
       });

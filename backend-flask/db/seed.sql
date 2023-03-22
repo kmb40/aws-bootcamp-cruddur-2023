cdef6d87372d342bfc1d1772033d147347bf2182
Kyle M. Brown <mayvik@gmail.com>
2023-03-22 14:43:59 -0400

    Update seed.sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,14 +2,14 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-  ('Kyle Brown','mayvik+cloudcamp@gmail.com' , 'kylembrown' ,'MOCK'), #inserting to match query with prod cognito user
+  ('Kyle Brown','mayvik+cloudcamp@gmail.com' , 'kylembrown' ,'MOCK'), --inserting to match query with prod cognito user
   ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
   ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'kylembrown' LIMIT 1), #changing to match query with prod cognito user
+    (SELECT uuid from public.users WHERE users.handle = 'kylembrown' LIMIT 1), --changing to match query with prod cognito user
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )

06e716586838a822855977bff8fc0f2efa1761b2
Kyle M. Brown <mayvik@gmail.com>
2023-04-06 03:53:30 +0000

    One bad MFing bug line 77 create_at

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -74,8 +74,7 @@ class Ddb:
       })
     return results
   def create_message(client,message_group_uuid, message, my_user_uuid, my_user_display_name, my_user_handle):
-    create_at = datetime.now().isoformat()
-    created_at = now
+    created_at = datetime.now().isoformat()
     message_uuid = str(uuid.uuid4())
 
     record = {

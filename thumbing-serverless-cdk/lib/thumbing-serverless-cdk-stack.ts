29b1ab623ecd9b534d3238566f84964c1fb07c7e
Kyle M. Brown <mayvik@gmail.com>
2023-04-11 01:09:54 +0000

    all work redone.

---
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -0,0 +1,59 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+import * as dotenv from 'dotenv';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+// dotenv.config();
+// Load env variables
+//const dotenv = require('dotenv')
+dotenv.config();
+
+export class ThumbingServerlessCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
+    const folderInput: string = process.env.THUMBING_S3_FOLDER_INPUT as string;
+    const folderOutput: string = process.env.THUMBING_S3_FOLDER_OUTPUT as string;
+ //   const webhookUrl: string = process.env.THUMBING_WEBHOOK_URL as string;
+ //   const topicName: string = process.env.THUMBING_TOPIC_NAME as string;
+ //   console.log('bucketName',bucketName)
+ //   console.log('folderInput',folderInput)
+ //   console.log('folderOutput',folderOutput)
+ //   console.log('webhookUrl',webhookUrl)
+ //   console.log('topicName',topicName)
+ //   console.log('functionPath',functionPath)    
+
+    const bucket = this.createBucket(bucketName);
+    const lambda = this.createLambda(folderInput,folderOutput,functionPath,bucketName)
+  
+  }
+
+  createBucket(bucketName: string): s3.IBucket{
+    const bucket = new s3.Bucket(this, 'ThumbingBucket', {
+      bucketName: bucketName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    return bucket;
+  }
+
+  createLambda(folderIntput: string, folderOutput: string, functionPath: string, bucketName: string): lambda.IFunction {
+    const lambdaFunction = new lambda.Function(this, 'ThumbLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(functionPath),
+      environment: {
+        DEST_BUCKET_NAME: bucketName,
+        FOLDER_INPUT: folderIntput,
+        FOLDER_OUTPUT: folderOutput,
+        PROCESS_WIDTH: '512',
+        PROCESS_HEIGHT: '512'
+        }
+    });
+    return lambdaFunction;
+  }
+}

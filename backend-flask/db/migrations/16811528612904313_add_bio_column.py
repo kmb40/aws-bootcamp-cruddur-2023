e8a526ba7cd2664e328e28e5f7b063b60f45a940
Kyle M. Brown <mayvik@gmail.com>
2023-04-13 23:09:48 +0000

    updates

--- backend-flask/db/migrations/16811528612904313_add_bio_column.py
+++ backend-flask/db/migrations/16811528612904313_add_bio_column.py
@@ -8,7 +8,7 @@ class AddBioColumnMigration:
     return data
   def rollback_sql():
     data = """
-      ALTER TABLE public.users DROP COLUMN;
+      ALTER TABLE public.users DROP COLUMN bio;
     """
     return data
 

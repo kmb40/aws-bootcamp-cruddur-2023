33f5b482a0928ca22d8ea85aabcdc2bbf48c3246
Kyle M. Brown <mayvik@gmail.com>
2023-04-13 09:19:19 -0400

    Update ProfileHeading.js

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -2,15 +2,21 @@ import './ProfileHeading.css';
 import EditProfileButton from '../components/EditProfileButton';
 
 export default function ProfileHeading(props) {
+  const backgroundImage = 'url("https://assets.cruddur.net/banners/SOLUSD_2021-11-06_06-14-51.png")';
+  const styles = {
+    backgroundImage: backgroundImage,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
   return (
    <div className='activity_feed_heading profile_heading'>
     <div className='title'>{props.profile.display_name}</div>
     <div className="cruds_count">{props.profile.cruds_count}Cruds</div>  
-
-    <div className="avatar">
+    <div className="avatar" styles={styles}>
+      <div className="avatar">
         <img src="https://assets.cruddur.net/avatars/geordi.jpg"/>
-        </div>
-
+      </div>
+    </div>
         <div className="display_name">{props.display_name}</div>
         <div className="handle">@{props.handle}</div>
 

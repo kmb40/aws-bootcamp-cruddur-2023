ef60f41f94c2c8c024afc235bdb26eb45b29b286
Kyle M. Brown <mayvik@gmail.com>
2023-04-15 15:51:35 -0400

    Update ProfileHeading.js

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -15,7 +15,7 @@ export default function ProfileHeading(props) {
     <div className='title'>{props.profile.display_name}</div>
     <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
     <div className="banner" style={styles} >
-      <ProfileAvatar id={props.profile.cognito_user_uuid} />
+    <ProfileAvatar id={props.profile.cognito_user_uuid} />
     </div>
     <div className="info">
       <div className='id'>

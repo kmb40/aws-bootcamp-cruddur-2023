f0e687796ccff97ab608fd8ac00cc66a219d11f2
Kyle M. Brown <mayvik@gmail.com>
2023-05-26 00:10:09 +0000

    Refactored to use decorator up to 19:33

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request
+from flask import request, g
 from flask_cors import CORS, cross_origin
 import os
 import sys
@@ -17,7 +17,7 @@ from services.create_message import *
 from services.show_activity import *
 from services.update_profile import *
 
-from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError, jwt_required
 
 # HoneyComb ---------
 from opentelemetry import trace
@@ -213,23 +213,30 @@ def data_create_message():
     app.logger.debug(e)
     return {}, 401
 
+def default_home_feed(e):
+  # unauthenicatied request
+  app.logger.debug(e)
+  app.logger.debug("unauthenicated")
+  data = HomeActivities.run()
+  return data, 200
 
 @app.route("/api/activities/home", methods=['GET'])
 #@xray_recorder.capture('activities_home')
+@jwt_required(on_error=default_home_feed)
 def data_home():
-  access_token = extract_access_token(request.headers)
-  try:
-    claims = cognito_jwt_token.verify(access_token)
-    # authenicatied request
-    app.logger.debug("authenicated")
-    app.logger.debug(claims)
-    app.logger.debug(claims['username'])
-    data = HomeActivities.run(cognito_user_id=claims['username'])
-  except TokenVerifyError as e:
-    # unauthenicatied request
-    app.logger.debug(e)
-    app.logger.debug("unauthenicated")
-    data = HomeActivities.run()
+# access_token = extract_access_token(request.headers)
+# try:
+#   claims = cognito_jwt_token.verify(access_token)
+#   # authenicatied request
+#   app.logger.debug("authenicated")
+#   app.logger.debug(claims)
+#   app.logger.debug(claims['username'])
+#   data = HomeActivities.run(cognito_user_id=claims['username'])
+# except TokenVerifyError as e:
+#   # unauthenicatied request
+#   app.logger.debug(e)
+#   app.logger.debug("unauthenicated")
+  data = HomeActivities.run(cognito_user_id=g.cognito_user_id)
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])

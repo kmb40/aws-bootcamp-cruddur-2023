575cd42fb48213cdd1433b4f5d5127bf52b255c9
Kyle M. Brown <mayvik@gmail.com>
2023-06-01 10:26:41 -0400

    Update ReplyForm.js

--- frontend-react-js/src/components/ReplyForm.js
+++ frontend-react-js/src/components/ReplyForm.js
@@ -19,25 +19,25 @@ export default function ReplyForm(props) {
   }
 
   const onsubmit = async (event) => {
-    console.log('replyActivity',props.activity)
     event.preventDefault();
     const url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${props.activity.uuid}/reply`
-    const payload_data = {
+    payload_data = {
       activity_uuid: props.activity.uuid,
       message: message
     }
-    post(url,payload_data,{
-      auth: true,
-      setErrors: setErrors,
-      success: function(data){
-        if (props.setReplies) {
-          props.setReplies(current => [data,...current]);
-        }
-        // reset and close the form
-        setCount(0)
-        setMessage('')
-        props.setPopped(false)
-      }
+    post(url,payload_data,setErrors,function(data){
+      // add activity to the feed
+      let activities_deep_copy = JSON.parse(JSON.stringify(props.activities))
+      let found_activity = activities_deep_copy.find(function (element) {
+        return element.uuid ===  props.activity.uuid;
+      });
+      found_activity.replies.push(data)
+
+      props.setActivities(activities_deep_copy);
+      // reset and close the form
+      setCount(0)
+      setMessage('')
+      props.setPopped(false)
     })
   }
 
@@ -56,7 +56,6 @@ export default function ReplyForm(props) {
       props.setPopped(false)
     }
   }
-
   if (props.popped === true) {
     return (
       <div className="popup_form_wrap reply_popup" onClick={close}>
